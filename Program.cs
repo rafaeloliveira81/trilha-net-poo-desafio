9446173b67cf03bdd47c775ed68848fa4f6e525f
Rafael Pereira de Oliveira <rafael.oliveira4@alunos.unis.edu.br>
2022-09-21 21:49:51 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("35999998888", "IPhone 10", "123123123", 64);
+Nokia nokia = new Nokia("35988998888", "Nokia 6532", "345345345", 128);
+
+Console.WriteLine("Smartphone Nokia:");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Trello");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone IPhone:");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Duolingo");
